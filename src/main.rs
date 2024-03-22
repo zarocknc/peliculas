50fa8debcad050acc3fecf1d04c258c5b1590365
Sebastian Nicolas <71535370+zarocknc@users.noreply.github.com>
2024-03-22 12:02:35 -0500

    poem setup wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::io::Error;
+use dotenv::dotenv;
+
+use poem::{listener::TcpListener, middleware::Cors, EndpointExt, Route};
+use poem_openapi::OpenApiService;
+
 mod db;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    let endpoints = ();
+    let api_service = OpenApiService::new(endpoints, "Peliculas API", "0.1.0").server("http://localhost:8080/api");
+    let ui = api_service.swagger_ui();
+    let app = Route::new().nest("/api", api_service)
+    .nest("/", ui)
+    .with(Cors::new());
+    poem::Server::new(TcpListener::bind("127.0.0.1:8080")).run(app).await
 }
